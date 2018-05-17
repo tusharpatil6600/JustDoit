2f438c22cd5b65dbe16a0c5a11b7d6ad8ecbd420
Sunrise <Sunrise@PUNECPU402.EIC.EINFOCHIPS.COM>
2018-05-17 16:27:45 +0530

    modified main to previous state

--- main.cpp
+++ main.cpp
@@ -7,7 +7,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Widget w;
     w.show();
-    cout << Hello git << endl;
-	cout<< adding commit message via github<< endl
     return a.exec();
 }
