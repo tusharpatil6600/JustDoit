709a3770e53f93f49c432a767fdc7b5cdb8542e3
Sunrise <Sunrise@PUNECPU402.EIC.EINFOCHIPS.COM>
2018-05-17 14:47:46 +0530

    Hello Git message added in Main code File modified as below: main.cpp
    Signed-off-by: Sunrise <Sunrise@PUNECPU402.EIC.EINFOCHIPS.COM>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "widget.h"
 #include <QApplication>
+#inlude <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
     w.show();
-
+    cout << Hello git << endl;
     return a.exec();
 }
