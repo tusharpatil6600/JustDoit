4264c1eb647d463e2fd5ab2bb428dbbc7be0319e
tusharpatil <tushar.patil@einfochips.com>
2018-10-28 10:48:09 +0530

    Git branch modified

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #inlude <iostream>
 #moving to final branch
 #agian changes in new branch
+#agian changes in new branch0
+
+//below changes must be merged in master from new branch
+#agian changes in new branch1
+#agian changes in new branch2
+#agian changes in new branch3
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
